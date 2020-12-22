b00d72a616171d8c9dff903db015aa1fb019cbba
SagarR.B <sagarrb1805@gmail.com>
2020-12-22 11:02:07 +0530

    facing issue while jumping

--- images/pygame3.py
+++ images/pygame3.py
@@ -29,18 +29,39 @@ class player(object):
         self.jumpCount = 10
         self.left = False
         self.right = False
+        self.standing = True
+
 
     def draw(self, win):
         if self.walkCount + 1 >= 27:
             self.walkCount = 0
-        if self.left:
-            win.blit(walkLeft[self.walkCount // 3], (self.x, self.y))
-            self.walkCount += 1
-        elif self.right:
-            win.blit(walkRight[self.walkCount // 3], (self.x, self.y))
-            self.walkCount += 1
+
+        if not self.standing:
+            if self.left:
+                win.blit(walkLeft[self.walkCount // 3], (self.x, self.y))
+                self.walkCount += 1
+            elif self.right:
+                win.blit(walkRight[self.walkCount // 3], (self.x, self.y))
+                self.walkCount += 1
         else:
-            win.blit(char, (self.x, self.y))
+            if self.right:
+                win.blit(walkRight[0], (self.x, self.y))
+            else:
+                win.blit(walkLeft[0], (self.x, self.y))
+
+
+class projectile(object):
+    def __init__(self, x, y, radius, color, facing):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.facing = facing
+        self.vel = 8 * facing
+
+    def draw(self, win):
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
+
 
 
 def redrawGameWindow():
@@ -74,13 +95,14 @@ while run:
         man.x += man.vel
         man.right = True
         man.left = False
+        man.standing = False
     elif keys[pygame.K_LEFT] and man.x > man.vel:
         man.x -= man.vel
         man.left = True
         man.right = False
+        man.standing = False
     else:
-        man.left = False
-        man.right = False
+        man.standing = True
         man.walkCount = 0
 
     clock.tick(27)
